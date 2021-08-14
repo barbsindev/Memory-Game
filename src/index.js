ed00c0b545e90c45cfd35a1e34091148199e8090
bb <>
2021-08-14 01:24:05 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const card = document.querySelectorAll(".card");
 const images = {
   dataset: [],
   index: [],
+  elements: [],
 };
 
 const score = document.querySelector("h2");
@@ -9,44 +10,61 @@ const score = document.querySelector("h2");
 card.forEach((item, index, card) => {
   item.addEventListener("click", (event) => {
     item.classList.add("flip");
-    console.log(item.children[1].dataset.cardid, item.children);
+    // console.log(item.children[1].dataset.cardid, item.children);
 
-    if (images.index.length > 1) {
+    if (images.index.length > 1 || item.classList.contains("matched")) {
       images.dataset.length = 0;
       images.index.length = 0;
+      images.elements.length = 0;
     }
 
     images.dataset.push(item.children[1].dataset.cardid);
     images.index.push(index);
-    console.log(images);
+
+    console.log(item.classList);
+    images.elements.push(item);
+
+    // images.index.length > 1 && images.dataset[0] === images.dataset[1]
+    //   ? (score.innerHTML = "It is a match!")
+    //   : images.index.length > 1 && images.dataset[0] !== images.dataset[1]
+    //   ? (score.innerHTML = "Its is not a match! Try again!")
+    //   : (score.innerHTML = "Let's play!");
 
     if (images.index.length > 1 && images.dataset[0] === images.dataset[1]) {
       console.log("match");
-      score.innerHTML = "It is a match!";
+      setTimeout(() => {
+        score.innerHTML = "It is a match!";
+      }, 800);
+
+      console.log(images, "match");
+      images.elements.forEach((els) => {
+        els.classList.add("matched");
+      });
     } else if (
       images.index.length > 1 &&
       images.dataset[0] !== images.dataset[1]
     ) {
       console.log("no match found");
-      score.innerHTML = "It's not a match. Try again!";
+      setTimeout(() => {
+        score.innerHTML = "It's not a match. Try again!";
+      }, 800);
+
+      console.log(images, "nomatch");
 
       const handle = setTimeout(() => {
-        console.log(item);
-        item.classList.remove("flip");
-        console.log(item);
-
+        console.log(images.dataset[0], "settimeout");
+        console.log(images, "time");
+        images.elements.forEach((els) => {
+          console.log(els.classList[1], "els");
+          els.classList.remove("flip");
+        });
         clearTimeout(handle);
-      }, 1500);
-      console.log(handle);
+      }, 1200);
+      setTimeout(() => {
+        score.innerHTML = "Let's play!";
+      }, 2000);
     } else {
-      score.innerHTML = " ";
+      score.innerHTML = "Let's play!";
     }
-    // }
-
-    // console.log(images);
-    //   setTimeout(() => {
-    //     console.log(card);
-    //     item.classList.remove("flip");
-    //   }, 2000);
   });
 });
